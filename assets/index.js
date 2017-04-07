5371067d2f20246169d67bf9259db6eb56ede522
Yefri Gonzalez <th3-cod3-1230@hotmail.com>
2017-04-07 17:44:48 -0400

    [Add] reset function for restart the game

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,13 @@
 let turn = false
 const multiplayer = false
+let gameOver = false
 
-document.addEventListener('click', toggleTurn)
+document.addEventListener('click', startGame)
 
-function function_name (argument) {
-	// body... 
-}
 
 function putCircule(ev, computer = false){
 	if(computer){
-		console.log(ev.classList.add('circule'))
+		ev.classList.add('circule')
 	}else {
 		ev.target.classList.add('circule')
 	}
@@ -30,7 +28,7 @@ function toggleTurn (ev) {
 			turn = true
 		}else{
 			putX(ev)
-			document.removeEventListener('click', toggleTurn)
+			document.removeEventListener('click', startGame)
 			setTimeout(() => { computerTurn() }, 250)
 		}
 	}
@@ -38,11 +36,38 @@ function toggleTurn (ev) {
 
 function computerTurn () {
 	const opts = document.querySelectorAll('[class="cell"]')
-	if(!opts){ return}
+	if(!opts.length){ 
+		gameOver = true
+		startGame()
+		return
+	}
 	const length = opts.length
 	const choice = Math.round(Math.random() * (length -1))
 	putCircule(opts[choice], true)
-	document.addEventListener('click', toggleTurn)
+	document.addEventListener('click', startGame)
 }
 
+function resetGame () {
+	const resetCell = document.getElementsByClassName('cell')
+	const length = resetCell.length
+	for(let i = 0; i < length; i++)
+	{
+		resetCell[i].className = 'cell'
+	}
+	gameOver = false
+	turn = false
+	document.addEventListener('click', startGame)
 
+}
+
+function startGame(ev){
+	if(gameOver){
+		let reset = confirm('Game Over. Do you wanna play again?')
+		if(reset){
+			resetGame()
+		}
+	}else{
+		toggleTurn(ev)
+	}
+
+}
